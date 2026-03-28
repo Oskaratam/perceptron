efb9d7e1043effe52b7e6f0ba7ae8d5badafac14
Oskaratam <oskaratamanenko2@gmail.com>
2026-03-28 20:41:56 +0100

    Perceptron train methods

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Perceptron perceptron = new Perceptron(4);
+        Perceptron perceptron = new Perceptron(4, new String[]{"setosa", "versicolor"});
         System.out.println(perceptron.predict(new double[]{6.9,3.1,4.9,1.5}));
     }
 }
--- Perceptron.java
+++ Perceptron.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 public class Perceptron {
     int dimensions;
@@ -5,13 +7,19 @@ public class Perceptron {
     double beta;
     double threshold;
     double[] weights;
+    int maxNumberOfEpochs;
+    HashMap<String, Integer> label_map;
 
-    public Perceptron(int dimensions) {
+    public Perceptron(int dimensions, String[] labels) {
         this.dimensions = dimensions;
+        this.label_map = new HashMap<>(Map.of(
+                labels[0], 1,
+                labels[1], 0
+        ));
         this.alpha = 0.1;
         this.beta = 0.7;
         this.threshold = 0.1;
-
+        this.maxNumberOfEpochs = 10;
         this.weights = new double[this.dimensions];
         for(int i = 0; i < dimensions; i++) {
             weights[i] = Math.random() * 2 - 1;
@@ -25,7 +33,35 @@ public class Perceptron {
         }
         return net - this.threshold >= 0 ? 1 : 0;
     }
-    public void train(){
 
+    private void adjustWeigths(int desiredResult, int realResult, double[] input) {
+        for (int i = 0; i < this.weights.length; i++) {
+            this.weights[i] += input[i] * this.alpha * (desiredResult - realResult);
+        }
+    }
+
+    private void adjustThreshold(int desiredResult, int realResult) {
+        this.threshold -= (desiredResult - realResult) * this.alpha;
+    }
+
+    public void train(double[][] inputs, String[] labels) {
+        if (inputs.length != labels.length) { throw new IllegalArgumentException("Inputs and labels array must have same length"); }
+        int correctAnswers = 0;
+        int numberOfEpochs = 0;
+
+        while (correctAnswers < inputs.length && numberOfEpochs < this.maxNumberOfEpochs) {
+            correctAnswers = 0;
+            for (int i = 0; i < inputs.length; i++) {
+                int desiredResult = label_map.get(labels[i]);
+                int realResult = predict(inputs[i]);
+                if(desiredResult == realResult) {
+                    correctAnswers++;
+                } else {
+                    adjustWeigths(desiredResult, realResult, inputs[i]);
+                    adjustThreshold(desiredResult, realResult);
+                }
+            }
+            numberOfEpochs++;
+        }
     }
 }
